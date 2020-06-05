868fcb0f2aa658251cb97530a42ea689f36a0c33
Gianfranco Dutka <gdutk007@gmail.com>
2020-06-05 12:58:14 -0400

    removed typo.

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to my Page!.
+          Welcome to my Page!
         </p>
         <a
           className="App-link"
