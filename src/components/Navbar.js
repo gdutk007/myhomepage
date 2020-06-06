e38329208cfe9c5f41b2ea8a868674e2ba9304be
Gianfranco Dutka <gdutk007@gmail.com>
2020-06-06 01:57:00 -0400

    Added Bootstrap support. Navbar is looking better now.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,25 @@
 import React from 'react'
 import "../CSS/navbar.css"
+import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar'
+
+/* <header>
+<div>
+    <ul className="myname-navbar-ul">
+        <li className="left-navbar-list-item" ><a href="#home">Gianfranco Dutka</a></li>
+    </ul>
+</div>
+ <navbar class="nav">            
+    <ul className="navbar-unordered-list">
+        <li className="navbar-list-item" ><a href="#home">Home</a></li>
+        <li className="navbar-list-item" ><a href="#news">News</a></li>
+        <li className="navbar-list-item" ><a href="#contact">Contact</a></li>
+        <li className="navbar-list-item" ><a href="#about">About</a></li>
+    </ul>
+</navbar>
+</header> */
+
+
 
 function MyNavbar(){
     const myStyles = {
@@ -8,19 +28,18 @@ function MyNavbar(){
     }
     return(
         <div>
-            <header>
-                <div>
-                    <li className="left-navbar-list-item" ><a href="#home">Gianfranco Dutka</a></li>
-                </div>
-                 <nav>            
-                    <ul className="navbar-unordered-list">
-                        <li className="navbar-list-item" ><a href="#home">Home</a></li>
-                        <li className="navbar-list-item" ><a href="#news">News</a></li>
-                        <li className="navbar-list-item" ><a href="#contact">Contact</a></li>
-                        <li className="navbar-list-item" ><a href="#about">About</a></li>
-                    </ul>
-                </nav>
-            </header>
+            <Navbar  bg="dark" variant="dark">
+                <Nav className="mr-auto-1">
+                    <Nav.Link className="left-navbar-link" href="#home" >Gianfranco Dutka</Nav.Link>
+                </Nav>
+                <Nav className="mr-auto">
+                    <Nav.Link className="navbar-links" href="#home"> About </Nav.Link>
+                    <Nav.Link className="navbar-links" href="#home"> Portfolio </Nav.Link>
+                    <Nav.Link className="navbar-links" href="#home"> Contact </Nav.Link>
+                </Nav>
+            </Navbar>
+            <br />
+            <br />
         </div>
     )
 }
