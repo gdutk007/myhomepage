1e5f460c27ab48ed775710d611a9a425c92aedc7
Gianfranco Dutka <gdutk007@gmail.com>
2020-06-06 11:34:09 -0400

    Removed Bootstrap html. Used only html5 and css for Navbar. Now need to continue fixing the rest of the page.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,45 +1,21 @@
 import React from 'react'
 import "../CSS/navbar.css"
-import Nav from 'react-bootstrap/Nav'
-import Navbar from 'react-bootstrap/Navbar'
-
-/* <header>
-<div>
-    <ul className="myname-navbar-ul">
-        <li className="left-navbar-list-item" ><a href="#home">Gianfranco Dutka</a></li>
-    </ul>
-</div>
- <navbar class="nav">            
-    <ul className="navbar-unordered-list">
-        <li className="navbar-list-item" ><a href="#home">Home</a></li>
-        <li className="navbar-list-item" ><a href="#news">News</a></li>
-        <li className="navbar-list-item" ><a href="#contact">Contact</a></li>
-        <li className="navbar-list-item" ><a href="#about">About</a></li>
-    </ul>
-</navbar>
-</header> */
-
-
 
 function MyNavbar(){
-    const myStyles = {
-        color: "#FF8C00",
-        backgroundColor: "black"
-    }
+
     return(
         <div>
-            <Navbar  bg="dark" variant="dark">
-                <Nav className="mr-auto-1">
-                    <Nav.Link className="left-navbar-link" href="#home" >Gianfranco Dutka</Nav.Link>
-                </Nav>
-                <Nav className="mr-auto">
-                    <Nav.Link className="navbar-links" href="#home"> About </Nav.Link>
-                    <Nav.Link className="navbar-links" href="#home"> Portfolio </Nav.Link>
-                    <Nav.Link className="navbar-links" href="#home"> Contact </Nav.Link>
-                </Nav>
-            </Navbar>
-            <br />
-            <br />
+            <header>
+                <nav>
+                <ul className="top-header-nav-ul">
+                    <li className="top-left-header-nav-li"><a href="#home">Gianfranco Dutka</a></li>
+                    <li className="top-right-header-nav-li"><a href="#home">Home</a></li>
+                    <li className="top-right-header-nav-li"><a href="#news">News</a></li>
+                    <li className="top-right-header-nav-li"><a href="#contact">Contact</a></li>
+                    <li className="top-right-header-nav-li"><a href="#about">About</a></li>
+                </ul>
+                </nav>
+            </header>
         </div>
     )
 }
