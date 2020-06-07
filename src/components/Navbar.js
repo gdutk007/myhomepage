a178b82412ee65a4761d8f49601df6aff0926d90
Gianfranco Dutka <gdutk007@gmail.com>
2020-06-07 14:31:43 -0400

    Corrected the non-fixed navbar. It was working all along...

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,38 @@
 import React from 'react'
+import {useEffect, useState, useRef} from 'react'
 import "../CSS/navbar.css"
 
 function MyNavbar(){
-
+    // const [isSticky, setSticky] = useState(false);
+    // useEffect(()=>{
+    //     const header = document.getElementById("mynavbar");
+    //     const sticky = header.offsetTop;
+    //     const scrollCallBack = window.addEventListener("scroll",()=>{    
+    //         if(window.pageYOffset > sticky){
+    //             //header.classList.add("sticky");
+    //             setSticky(true);
+    //             console.log("sticky is true");
+    //         }else{
+    //             //header.classList.remove("sticky");
+    //             setSticky(false);
+    //             console.log("sticky is true");
+    //         }
+    //     });
+    //     return ()=> {
+    //         window.removeEventListener("scroll",scrollCallBack)
+    //     };
+    // },[])
     return(
         <div>
             <header>
-                <nav>
-                <ul className="top-header-nav-ul">
-                    <li className="top-left-header-nav-li"><a href="#home">Gianfranco Dutka</a></li>
-                    <li className="top-right-header-nav-li"><a href="#home">Home</a></li>
-                    <li className="top-right-header-nav-li"><a href="#news">News</a></li>
-                    <li className="top-right-header-nav-li"><a href="#contact">Contact</a></li>
-                    <li className="top-right-header-nav-li"><a href="#about">About</a></li>
-                </ul>
+                <nav id="mynavbar" className="sticky">
+                    <ul className="top-header-nav-ul">
+                        <li className="top-left-header-nav-li"><a href="#home">Gianfranco Dutka</a></li>
+                        <li className="top-right-header-nav-li"><a href="#home">Home</a></li>
+                        <li className="top-right-header-nav-li"><a href="#news">News</a></li>
+                        <li className="top-right-header-nav-li"><a href="#contact">Contact</a></li>
+                        <li className="top-right-header-nav-li"><a href="#about">About</a></li>
+                    </ul>
                 </nav>
             </header>
         </div>
--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Navbar from './components/Navbar'
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+
 ReactDOM.render(
   <React.StrictMode>
-    <Navbar/>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
