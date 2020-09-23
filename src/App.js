e6758cce50095bbeed6c3569bb6f984f69e583c2
Gianfranco <gdutk007@gmail.com>
2020-09-22 21:38:14 -0400

    messing with git in new computer.

--- src/App.js
+++ src/App.js
@@ -25,6 +25,7 @@ function App() {
         <br/>
         <body>
         <p style={{color: "white"}}>
+
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
       tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
       veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
